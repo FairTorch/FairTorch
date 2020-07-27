b5b596bbcbd02a9cc95c903d651282d6811a3154
maxhirsch <mhirsch06@gmail.com>
2020-07-27 01:11:20 -0400

    no install pip

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -17,7 +17,7 @@ set -x
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python-pip
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme #python-pip
  
 #####################
 # DECLARE VARIABLES #
