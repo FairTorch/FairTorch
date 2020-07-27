b4be77469bd8659be05b5b32db2203da4c7be89c
maxhirsch <mhirsch06@gmail.com>
2020-07-27 00:37:31 -0400

    pip3 -> pip

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,7 @@ set -x
  
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
-pip3 install -r requirements.txt
+pip install -r requirements.txt
  
 #####################
 # DECLARE VARIABLES #
