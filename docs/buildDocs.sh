d5944111aa73134898a45fe352c88bddd89474fc
maxhirsch <mhirsch06@gmail.com>
2020-07-27 00:45:00 -0400

    updated requirements path

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,7 @@ set -x
  
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python-pip
-pip install -r requirements.txt
+pip install -r docs/requirements.txt
  
 #####################
 # DECLARE VARIABLES #
