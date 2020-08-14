2f6ffa5d294df8c505377fedf70bf8d518589aab
nadinem100 <67973324+nadinem100@users.noreply.github.com>
2020-08-13 20:54:37 -0400

    first 4 metrics

--- api/evaluation/metrics.py
+++ api/evaluation/metrics.py
@@ -8,62 +8,46 @@ Parameter definitions:
 
     :param pred_labels: predicted output from model as a 1D array
     :param true_labels: the true labels as a 1D array (same size as pred_labels)
-    :param classes: the groups to classify an object as a 1D array of integers
+    :param classes: the group of each element
 
 """
 
 import numpy as np
 
 def pred_pos(pred_labels, true_labels, classes):
-    """
-    :return: integer array of total positive predictions for each group
-    """
-
-    class_correct = np.zeros(len(classes))
-
-    for i, c in enumerate(pred_labels):
-        class_correct[c] += 1
-
-    return class_correct
-
-def pred_neg(pred_labels, true_labels, classes):
-    """
-    :return: integer array of total negative predictions in each group
-    """
-    
-    b = pred_pos(pred_labels, true_labels, classes)
-    a = np.empty(len(classes))
-    a.fill(len(pred_labels))    
-    return a-b #don't know what the purpose of this metric is
-
-def pred_prevalence(pred_labels, true_labels, classes):
-    return pred_pos(pred_labels, true_labels, classes)
-
-def pred_pos_rate(pred_labels, true_labels, classes):
-    #this is the same as ppr since we are not having input as binary correct....
-    
-def total_pred_pos(pred_labels):
-    """
-    :return: integer of total correct predictions
-    """
-    #pred_labels = np.argmax(classification_scores, axis=1) #finds max along each row
-    #return np.sum(pred_labels == true_labels) <-- seems like useful metric, but not this specific one
-
-    return len(pred_labels) #this seems so wrong
-    
-
-def true_pos(pred_labels, true_labels, classes):
-    """
-    :return: integer array of total correct predictions in each group
-    """
     correct = pred_labels == true_labels
-    class_correct = np.zeros(len(classes))
+    class_correct = np.zeros(np.unique(classes));
 
     for i, c in enumerate(correct):
         if c:
             class_correct[true_labels[i]] += 1
-    
+
     return class_correct
+
+def total_pred_pos(pred_labels, true_labels):
+    """true_labels is 1D array"""
+    return np.sum(pred_labels == true_labels)
+
+def pred_neg(pred_labels, true_labels, classes):
+    correct = pred_labels != true_labels
+    class_correct = np.zeros(np.unique(classes));
+
+    for i, c in enumerate(correct):
+        if c:
+            class_correct[true_labels[i]] += 1
+
+    return class_correct
+
+def pred_prevalence(pred_labels, true_labels, classes):
+    class_total = np.zeros(np.unique(classes));
+
+    for i, c in enumerate(classes):
+        class_total[classes[i]] += 1 #only works if classes is numbers
+
+    return pred_pos(pred_labels, true_labels, classes) / class_total(pred_labels);
+
+def pred_pos_rate(pred_labels, true_labels, classes):
+    return pred_pos(pred_labels, true_labels, classes) / total_pred_pos;
 
         
 classification_scores = np.array([[ 30,   1,  10,  80],
