dc27d47e893d8ba171f8d80dccde5fb6570ddda7
maxhirsch <mhirsch06@gmail.com>
2020-08-18 23:32:09 -0400

    trying again

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,7 @@ set -x
  
 apt-get update
 apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python-pip
-pip install mock
+pip3 install mock
 
 #####################
 # DECLARE VARIABLES #
