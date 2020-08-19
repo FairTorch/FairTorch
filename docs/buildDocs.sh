d3c01d7cfe641e4726a85f9ab92812d0bf941d08
maxhirsch <mhirsch06@gmail.com>
2020-08-18 23:27:03 -0400

    pip install mock

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -17,8 +17,9 @@ set -x
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme #python-pip
- 
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python-pip
+pip install mock
+
 #####################
 # DECLARE VARIABLES #
 #####################
