2cca28d0dace0b8cf39827192c57ef19a50d9714
michellexu23 <mimixu100@gmail.com>
2020-08-25 15:17:28 -0400

    recover monitor

--- fairtorch/training/monitor.py
+++ fairtorch/training/monitor.py
@@ -2,26 +2,78 @@
 Monitor
 =======
 
-A wrapper class for a neural network model which computes fairness metrics with
+A wrapper class for a neural network model which plots fairness metrics with
 the forward pass.
+
+
+Attributes:
+
+
+	:param model:	the PyTorch Model Monitor is being used with
+	:param metric_names:	list of the names of the metrics used
+	:param metric_methods:	list of functions to evaluate metrics in the same order as metric_names
+	:param groups:	list of the groups of data 
+	:param fig:	Matplotlib Pyplot figure with subplots of the value of each metric over training steps
+
+
+Parameter definitions:
+
+
+	:param model:	PyTorch Model which is being trained
+	:param metrics_dict:	dictionary with name and function for each metric to be plotted
+	:param groups:	the groups of data as a list
+	:step_no: number of current training step (e.g. mini-batch number, batch number, epoch number)
+	:param data: validation data as list of inputs to model
+	:param data_groups: the group of each element in data as a 1D array
+	:param labels: labels for each element in data as a 1D array
+
 
 """
 import torch
 import torch.nn as nn
-from torch.nn import Module
+import matplotlib.pyplot as plt
+import numpy as np
 
+class Monitor:
+	def __init__(self, model, metrics_dict, groups): 	
+		self.model = model
+		self.metric_names = []
+		self.metric_methods = []
+		for name, method in metrics_dict.items():
+		    self.metric_names.append(name)
+			self.metric_methods.append(method)
+		self.groups = groups
+		self.fig, axs = plt.subplots(len(metrics_dict))
+		for m in range(len(self.metric_names)):
+			axs[m].set_xlabel('Training Step')
+			axs[m].set_ylabel(metric_names[m])
+			for g in groups:
+				axs[m].plot([],[], label = g)
 
-class Monitor(Module):
-    """
-    Bases: :class:`torch.nn.Module`
-    """
-    def __init__(self, model):
-        super(Monitor, self).__init__()
-        self.model = model
-
-    def forward(self, x):
-        """
-        Forward pass of model
-        """
-        return self.model(x)
-
+	def update_fig(step_no, data, data_groups, labels):
+    	"""
+		Update figure at the end of a foward pass with values of metrics for each group and display the figure
+		"""
+		
+		scores = self.model(data)
+		axs = self.fig.get_axes()
+		for m in range(len(self.metric_names)):
+			lines = axs[m].get_lines()
+			new_ydata = self.metric_methods[m](scores, labels, data_groups)
+			for g in range(len(groups)):
+    			lines[g].set_xdata(np.append(lines[g].get_xdata(), step_no))
+				lines[g].set_ydata(np.append(lines[g].get_ydata(), new_ydata[groups[g]]))
+			axs[m].relim()
+			axs[m].autoscale_view()
+			axs[m].legend()
+		plt.draw()
+		fig.show()
+	
+	def get_fig():
+		"""
+			Get fig attribute
+			
+			
+			:return: Matplotlib Pyplot figure with subplots corresponding to each metric
+		"""
+		return self.fig
