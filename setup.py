3021a649fdeaf10acee5972f8c09d3868a790d43
Joyce Zhang <joyce@Joyces-MacBook-Air-2.local>
2020-08-24 20:43:37 -0700

    made setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name=‘FairTorch’,
+    version=‘0.1.0’,
+    description=‘A library for fair machine learning created for the PyTorch Summer Hackathon 2020.’,
+    url=‘https://github.com/FairTorch/FairTorch’,
+    author=‘FairTorch’,
+    author_email='fairtorch@gmail.com’,
+    license='MIT License',
+    packages=['FairTorch'],
+    install_requires=['numpy', 
+                      'torch',
+                      'future'
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
